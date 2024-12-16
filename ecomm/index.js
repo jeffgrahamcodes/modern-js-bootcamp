0c2709f22faca840fd4e1074135502c6c10c5372
J. Graham <polymathy78@gmail..com>
2024-12-16 16:47:28 -0500

    add middleware

--- ecomm/index.js
+++ ecomm/index.js
@@ -3,7 +3,38 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hi!');
+  res.send(`
+    <div>
+    <form method="POST" action="">
+      <input name="email" type="text" placeholder="email">
+      <input name="password" type="text" placeholder="password">
+      <input name="passwordConfirmation" type="text" placeholder="password confirmatiom">
+      <button>Sign Up</button>
+    </form>
+  </div>
+    `);
+});
+
+const bodyParser = (req, res, next) => {
+  if (req.method === 'POST') {
+    req.on('data', (data) => {
+      const parsed = data.toString('utf8').split('&');
+      const formData = {};
+      for (let pair of parsed) {
+        const [key, value] = pair.split('=');
+        formData[key] = value;
+      }
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+app.post('/', bodyParser, (req, res) => {
+  console.log(req.body);
+  res.send('Account created');
 });
 
 app.listen(3000, () => {
