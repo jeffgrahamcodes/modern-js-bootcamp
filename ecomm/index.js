6dcbb52dfd3f5d068931985653284e4885fab453
J. Graham <polymathy78@gmail..com>
2024-12-16 13:06:39 -0500

    create web server

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,1 +1,11 @@
-console.log('hi');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hi!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening...');
+});
