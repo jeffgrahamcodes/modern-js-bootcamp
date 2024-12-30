d6b806a15c80c9a36a1ebfb242fd38b8b749172a
J. Graham <polymathy78@gmail..com>
2024-12-29 22:14:32 -0500

    add middleware for handling errors

---
+++ ecomm/routes/admin/middlewares.js
@@ -0,0 +1,15 @@
+const { validationResult } = require('express-validator');
+
+module.exports = {
+  handleErrors(templateFx) {
+    return (req, res, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.send(templateFx({ errors }));
+      }
+
+      next();
+    };
+  },
+};
--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
 const multer = require('multer');
 
 const productsRepo = require('../../repositories/products');
 const newProductsTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
+const { handleErrors } = require('./middlewares');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -18,13 +18,8 @@ router.post(
   '/admin/products/new',
   upload.single('image'),
   [requireTitle, requirePrice],
+  handleErrors(newProductsTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(newProductsTemplate({ errors }));
-    }
-
     const image = req.file.buffer.toString('base64');
     const { title, price } = req.body;
 
