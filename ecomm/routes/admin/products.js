cce14a496323688ccbdcc3744136269ed9e8b43e
J. Graham <polymathy78@gmail..com>
2024-12-30 11:21:33 -0500

    require auth

--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -5,22 +5,23 @@ const productsRepo = require('../../repositories/products');
 const newProductsTemplate = require('../../views/admin/products/new');
 const renderedProductsTemplate = require('../../views/admin/products');
 const { requireTitle, requirePrice } = require('./validators');
-const { handleErrors } = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', async (req, res) => {
+router.get('/admin/products', requireAuth, async (req, res) => {
   const products = await productsRepo.getAll();
   res.send(renderedProductsTemplate({ products }));
 });
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
   res.send(newProductsTemplate({}));
 });
 
 router.post(
   '/admin/products/new',
+  requireAuth,
   upload.single('image'),
   [requireTitle, requirePrice],
   handleErrors(newProductsTemplate),
