8bace47280247154eb7c29fc44b963ae6406d38d
J. Graham <polymathy78@gmail..com>
2024-12-29 22:32:40 -0500

    add view for product listing

--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -3,13 +3,18 @@ const multer = require('multer');
 
 const productsRepo = require('../../repositories/products');
 const newProductsTemplate = require('../../views/admin/products/new');
+const renderedProductsTemplate = require('../../views/admin/products');
 const { requireTitle, requirePrice } = require('./validators');
 const { handleErrors } = require('./middlewares');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {});
+router.get('/admin/products', async (req, res) => {
+  const products = await productsRepo.getAll();
+  res.send(renderedProductsTemplate({ products }));
+});
+
 router.get('/admin/products/new', (req, res) => {
   res.send(newProductsTemplate({}));
 });
