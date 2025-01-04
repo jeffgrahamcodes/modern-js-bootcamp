47b070e51e325dfb03f8ef973adf9422db0139b6
J. Graham <polymathy78@gmail..com>
2025-01-04 17:16:26 -0500

    edit products

--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -53,7 +53,24 @@ router.get(
 router.post(
   '/admin/products/:id/edit',
   requireAuth,
-  async (req, res) => {}
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  handleErrors(editProductTemplate),
+  async (req, res) => {
+    const changes = req.body;
+
+    if (req.file) {
+      changes.image = req.file.buffer.toString('base64');
+    }
+
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (error) {
+      return res.send('Could not find item');
+    }
+
+    res.redirect('/admin/products');
+  }
 );
 
 module.exports = router;
