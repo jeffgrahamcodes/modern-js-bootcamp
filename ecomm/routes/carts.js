61d4be3ee41e897535df9244b26af55e5d5587e6
J. Graham <polymathy78@gmail..com>
2025-01-04 20:38:29 -0500

    total cart items

--- ecomm/routes/carts.js
+++ ecomm/routes/carts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
+const productRepo = require('../repositories/products');
+const showCartTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -27,6 +29,23 @@ router.post('/cart/products', async (req, res) => {
   });
 
   res.send('product added to cart');
+});
+
+router.get('/cart', async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect('/');
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+  console.log(cart);
+
+  for (let item of cart.items) {
+    const product = await productRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(showCartTemplate({ items: cart.items }));
 });
 
 module.exports = router;
